4415a02d276bfb5aeb2c35ffdbea0365c3ce6ea5
federico-casu <federico.casu1101@gmail.com>
2024-01-10 16:02:43 +0100

    feat(mail game): added input interface

--- Mail game/main.js
+++ Mail game/main.js
@@ -7,21 +7,35 @@ stampa un messaggio appropriato sull’esito del controllo.
 
 ------------------------------------------------------------------------------- */
 
-const userMail = prompt('Inserisci la tua mail');
-const mailingList = ["marco@live.it", "giovanni@gmail.com", "andrea@hotmail.com", "luca@gmail.com", "gabriele@live.it"];
-let flag = false;
+//const userMail = prompt('Inserisci la tua mail');
+const buttonHtml = document.querySelector('button');
+const badgeSuccess = document.querySelector('div.container.bg-success');
+const badgeDenied = document.querySelector('div.container.bg-danger');
 
+buttonHtml.addEventListener('click', function(){
+    const mailingList = ["marco@live.it", "giovanni@gmail.com", "andrea@hotmail.com", "luca@gmail.com", "gabriele@live.it"];
+    let flag = false;
+    const userMailHtml = document.getElementById('userMail');
 
-for (let i = 0; i < mailingList.length; i++) {
-    const element = mailingList[i];
-
-    if (userMail === element) {
-        flag = true;
+    //console.log(userMailHtml.value);
+    
+    for (let i = 0; i < mailingList.length; i++) {
+        const element = mailingList[i];
+    
+        if (userMailHtml.value === element) {
+            flag = true;
+        }
     }
-}
 
-if (flag == true) {
-    console.log('Accesso eseguito')
-} else {
-    console.log('Accesso negato')
-}
+    if (flag == true) {
+        //console.log('Accesso eseguito');
+        badgeSuccess.classList.remove('d-none');
+        badgeSuccess.classList.add('d-block');
+    } else {
+        //console.log('Accesso negato');
+        badgeDenied.classList.remove('d-none');
+        badgeDenied.classList.add('d-block');
+    }
+
+})
+
